7c4b442c0e5683799d109bfcc323f00a3fb5a920
Shivam Dewangan <161064463+shivam-dewangan@users.noreply.github.com>
2024-09-15 13:10:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
-import messageRoutes from './routes/messageRoutes.js';
+import messageRoutes from './routes/MessageRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
